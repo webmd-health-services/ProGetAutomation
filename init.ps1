746c7c02196df3b37b4d1d205c2cb3e8ea1aa99f
Scott Pickthorn <spickthorn@webmd.net>
2017-11-03 10:08:46 -0700

    Updating proget version to 4.8.7

--- init.ps1
+++ init.ps1
@@ -20,7 +20,7 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'Carbon') -Force -
 
 $runningUnderAppVeyor = (Test-Path -Path 'env:APPVEYOR')
 
-$version = '4.7.14'
+$version = '4.8.7'
 
 $installerPath = Join-Path -Path $env:TEMP -ChildPath ('ProGetInstaller-SQL-{0}.exe' -f $version)
 if( -not (Test-Path -Path $installerPath -PathType Leaf) )
