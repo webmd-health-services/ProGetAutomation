f868ebb9e052018d76fad8667c64b4f639d39ff7
Jeffrey Hatch <jhatch@webmd.net>
2024-02-12 20:52:05 -0800

    Migrate Get-ProGetUniversalPackage.Tests to Pester 5

--- Tests/Get-ProGetUniversalPackage.Tests.ps1
+++ Tests/Get-ProGetUniversalPackage.Tests.ps1
@@ -2,89 +2,84 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
+BeforeAll {
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
 
-$session = New-ProGetTestSession
-$result = $null
-$upackFile = $null
-$feedName = 'Get-ProGetUniversalPackage.Tests.ps1'
+    $session = New-ProGetTestSession
+    $result = $null
+    $upackFile = $null
+    $feedName = 'Get-ProGetUniversalPackage.Tests.ps1'
 
-function GivenFeed
-{
-    param(
-        $Name
-    )
-
-    Get-ProGetFeed -Session $Session -Name $Name -ErrorAction Ignore | Remove-ProGetFeed -Session $Session -Force
-    New-ProGetFeed -Session $session -Name $Name -Type 'Universal'
-}
-
-function GivenPackage
-{
-    param(
-        [Parameter(Mandatory,Position=0)]
-        [string]
-        $Named,
-
-        [string]
-        $InGroup
-    )
-
-    $params = @{ }
-    if( $InGroup )
+    function GivenFeed
     {
-        $params['GroupName'] = $InGroup
+        param(
+            $Name
+        )
+
+        Get-ProGetFeed -Session $Session -Name $Name -ErrorAction Ignore | Remove-ProGetFeed -Session $Session -Force
+        New-ProGetFeed -Session $session -Name $Name -Type 'Universal'
     }
 
-    $script:upackFile = New-ProGetUniversalPackage -OutFile (Join-Path -Path $TestDrive.FullName -ChildPath ('{0}{1}.zip' -f $InGroup,$Named)) -Version '0.0.0' -Name $Named @params
-    Publish-ProGetUniversalPackage -Session $session -FeedName $feedName -PackagePath $upackFile
-}
+    function GivenPackage
+    {
+        param(
+            [Parameter(Mandatory,Position=0)]
+            [string]
+            $Named,
 
-function Init
-{
-    $script:result = $null
-    $script:upackFile = $null
-    GivenFeed $feedName
-}
+            [string]
+            $InGroup
+        )
 
-function ThenError
-{
-    param(
-        $Matches
-    )
+        $params = @{ }
+        if( $InGroup )
+        {
+            $params['GroupName'] = $InGroup
+        }
 
-    It ('should write error') {
+        $filepath = Join-Path -Path $TestDrive -ChildPath ('package.{0}.upack' -f [IO.Path]::GetRandomFileName())
+        $upackFile = New-ProGetUniversalPackage -OutFile $filepath -Version '0.0.0' -Name $Named @params
+        Publish-ProGetUniversalPackage -Session $session -FeedName $feedName -PackagePath $upackFile
+    }
+
+    function Init
+    {
+        $script:result = $null
+        $script:upackFile = $null
+        GivenFeed $feedName
+    }
+
+    function ThenError
+    {
+        param(
+            $Matches
+        )
+
         $Global:Error | Should -Match $Matches
     }
-}
 
-function ThenNoErrors
-{
-    param(
-    )
+    function ThenNoErrors
+    {
+        param(
+        )
 
-    It ('should write no errors') {
         $Global:Error | Should -BeNullOrEmpty
     }
-}
 
-function ThenNothingReturned
-{
-    It ('should return nothing') {
+    function ThenNothingReturned
+    {
         $result | Should -BeNullOrEmpty
     }
-}
 
-function ThenPackageReturned
-{
-    param(
-        [string[]]
-        $Named,
+    function ThenPackageReturned
+    {
+        param(
+            [string[]]
+            $Named,
 
-        $InGroup = ''
-    )
-
-    It ('should return package') {
+            $InGroup = ''
+        )
+        $result = $script:result
         $result | Should -HaveCount $Named.Count
         foreach( $name in $Named )
         {
@@ -92,137 +87,130 @@ function ThenPackageReturned
             $result | Where-Object { $_.group -eq $InGroup } | Should -HaveCount $Named.Count
         }
     }
-}
 
-function WhenGettingPackage
-{
-    [CmdletBinding()]
-    param(
-        $Named,
-        $InGroup
-    )
-
-    $params = @{ }
-    if( $PSBoundParameters.ContainsKey('Named') )
+    function WhenGettingPackage
     {
-        $params['Name'] = $Named
+        [CmdletBinding()]
+        param(
+            $Named,
+            $InGroup
+        )
+
+        $params = @{ }
+        if( $PSBoundParameters.ContainsKey('Named') )
+        {
+            $params['Name'] = $Named
+        }
+        if( $PSBoundParameters.ContainsKey('InGroup') )
+        {
+            $params['GroupName'] = $InGroup
+        }
+        $Global:Error.Clear()
+        $script:result = Get-ProGetUniversalPackage -Session $session -FeedName $feedName @params
     }
-    if( $PSBoundParameters.ContainsKey('InGroup') )
-    {
-        $params['GroupName'] = $InGroup
-    }
-    $Global:Error.Clear()
-    $script:result = Get-ProGetUniversalPackage -Session $session -FeedName $feedName @params
 }
 
-Describe 'Get-ProGetUniversalPackage.when package doesn''t exist' {
-    Init
-    WhenGettingPackage 'Fubar' -ErrorAction SilentlyContinue
-    ThenNothingReturned
-    ThenError -Matches 'package\ was\ not\ found'
-}
-
-Describe 'Get-ProGetUniversalPackage.when package doesn''t exist and ignoring any errors' {
-    Init
-    WhenGettingPackage 'Fubar' -ErrorAction Ignore
-    ThenNothingReturned
-    ThenNoErrors
-}
-
-Describe 'Get-ProGetUniversalPackage.when getting a package' {
-    Init
-    GivenPackage 'Snafu'
-    WhenGettingPackage 'Snafu'
-    ThenPackageReturned 'Snafu'
-    ThenNoErrors
-}
-
-Describe 'Get-ProGetUniversalPackage.when getting all packages' {
-    Init
-    GivenPackage 'Snafu'
-    GivenPackage 'Fizz'
-    GivenPackage 'Buzz'
-    WhenGettingPackage
-    ThenPackageReturned 'Snafu','Fizz','Buzz'
-    ThenNoErrors
-}
-
-Describe 'Get-ProGetUniversalPackage.when getting with wildcard' {
-    Init
-    GivenPackage 'Snafu'
-    GivenPackage 'Fizz'
-    GivenPackage 'Buzz'
-    WhenGettingPackage '*zz'
-    ThenPackageReturned 'Fizz','Buzz'
-    ThenNoErrors
-}
-
-if( $false )
-{
-    # There's currently a bug in ProGet that doesn't allow this.
-    Describe 'Get-ProGetUniversalPackage.when getting all packages in a group' {
+Describe 'Get-ProGetUniversalPackage when package doesn''t exist' {
+    BeforeEach {
         Init
-        GivenPackage 'Snafu0'
-        GivenPackage 'Snafu1' -InGroup 'One'
-        GivenPackage 'Snafu21' -InGroup 'Two'
-        GivenPackage 'Snafu22' -InGroup 'Two'
-        GivenPackage 'Snafu23' -InGroup 'Two'
-        GivenPackage 'Snafu3' -InGroup 'Three'
-        WhenGettingPackage -InGroup 'Two'
-        ThenPackageReturned 'Snafu21','Snafu22','Snafu23' -InGroup 'Two'
+    }
+
+    It 'should return nothing and provide an error' {
+        WhenGettingPackage 'Fubar' -ErrorAction SilentlyContinue
+        ThenNothingReturned
+        ThenError -Matches 'package\ was\ not\ found'
+    }
+
+    It 'should return nothing and provide no error' {
+        WhenGettingPackage 'Fubar' -ErrorAction Ignore
+        ThenNothingReturned
         ThenNoErrors
     }
 }
 
-Describe 'Get-ProGetUniversalPackage.when getting package in a group and there are duplicate packages in other groups' {
-    Init
-    GivenPackage 'Snafu'
-    GivenPackage 'Snafu' -InGroup 'One'
-    GivenPackage 'Snafu' -InGroup 'Two'
-    GivenPackage 'Snafu' -InGroup 'Three'
-    WhenGettingPackage 'Snafu' -InGroup 'Three'
-    ThenPackageReturned 'Snafu' -InGroup 'Three'
-    ThenNoErrors
+Describe 'Get-ProGetUniversalPackage' {
+    BeforeEach {
+        Init
+    }
+
+    It 'should return package' {
+        GivenPackage 'Snafu'
+        WhenGettingPackage 'Snafu'
+        ThenPackageReturned 'Snafu'
+        ThenNoErrors
+    }
+
+    It 'should return all packages when name passed is empty string' {
+        GivenPackage 'Snafu1'
+        GivenPackage 'Snafu2'
+        WhenGettingPackage ''
+        ThenPackageReturned 'Snafu1','Snafu2'
+        ThenNoErrors
+    }
+
+    It 'should return all packages when no group or name provided' {
+        GivenPackage 'Snap'
+        GivenPackage 'Fizz'
+        GivenPackage 'Buzz'
+        WhenGettingPackage
+        ThenPackageReturned 'Snap','Fizz','Buzz'
+        ThenNoErrors
+    }
+
+    It 'should return packages using wildcard on passed name' {
+        GivenPackage 'Fizz'
+        GivenPackage 'Buzz'
+        GivenPackage 'Bizzes'
+        WhenGettingPackage '*zz'
+        ThenPackageReturned 'Fizz','Buzz'
+        ThenNoErrors
+    }
 }
 
-Describe 'Get-ProGetUniversalPackage.when getting getting packages by group using wildcards' {
-    Init
-    GivenPackage 'Snafu0'
-    GivenPackage 'Snafu1' -InGroup 'One'
-    GivenPackage 'Snafu2' -InGroup 'Two'
-    GivenPackage 'Snafu3' -InGroup 'Three'
-    WhenGettingPackage -InGroup 'O*'
-    ThenPackageReturned 'Snafu1' -InGroup 'One'
-    ThenNoErrors
-}
+Describe 'Get-ProGetUniversalPackage grab packages by group' {
+    BeforeEach {
+        Init
+    }
 
-Describe 'Get-ProGetUniversalPackage.when passing empty string to group' {
-    Init
-    GivenPackage 'Snafu'
-    GivenPackage 'Snafu' -InGroup 'One'
-    GivenPackage 'Snafu' -InGroup 'Two'
-    GivenPackage 'Snafu' -InGroup 'Three'
-    WhenGettingPackage 'Snafu' -InGroup ''
-    ThenPackageReturned 'Snafu'
-    ThenNoErrors
-}
+    It 'should only return package within provided group when duplicate named packages are in other groups' {
+        GivenPackage 'Snafu'
+        GivenPackage 'Snafu' -InGroup 'One'
+        GivenPackage 'Snafu' -InGroup 'Two'
+        GivenPackage 'Snafu' -InGroup 'Three'
+        WhenGettingPackage 'Snafu' -InGroup 'Three'
+        ThenPackageReturned 'Snafu' -InGroup 'Three'
+        ThenNoErrors
+    }
 
-Describe 'Get-ProGetUniversalPackage.when passing empty string to name' {
-    Init
-    GivenPackage 'Snafu1'
-    GivenPackage 'Snafu2'
-    WhenGettingPackage ''
-    ThenPackageReturned 'Snafu1','Snafu2'
-    ThenNoErrors
-}
+    It 'should return all packages within a group' {
+        GivenPackage 'Snafu'
+        GivenPackage 'Snafu' -InGroup 'One'
+        GivenPackage 'Snafu' -InGroup 'Two'
+        GivenPackage 'Snafu22' -InGroup 'Two'
+        GivenPackage 'Snafu23' -InGroup 'Two'
+        GivenPackage 'Snafu' -InGroup 'Three'
+        WhenGettingPackage -InGroup 'Two'
+        ThenPackageReturned 'Snafu','Snafu22','Snafu23' -InGroup 'Two'
+        ThenNoErrors
+    }
 
-Describe 'Get-ProGetUniversalPackage.when searching for packages with the same name across groups with group wildcard' {
-    Init
-    GivenPackage 'Snafu'
-    GivenPackage 'Snafu' -InGroup 'One'
-    GivenPackage 'Snafu' -InGroup 'Two'
-    GivenPackage 'Snafu' -InGroup 'Three'
-    WhenGettingPackage 'Snafu' -InGroup 'O*'
-    ThenPackageReturned 'Snafu' -InGroup 'One'
-    ThenNoErrors
+    It 'should return all packages within a group using a wildcard on the group name' {
+        GivenPackage 'Snafu'
+        GivenPackage 'Snafu' -InGroup 'One'
+        GivenPackage 'Snafu' -InGroup 'Two'
+        GivenPackage 'Snafu' -InGroup 'Three'
+        WhenGettingPackage -InGroup 'O*'
+        ThenPackageReturned 'Snafu' -InGroup 'One'
+        ThenNoErrors
+    }
+
+    It 'should return same named package that is not in group when group passed is an empty string' {
+        GivenPackage 'Snafu'
+        GivenPackage 'Snafu' -InGroup 'One'
+        GivenPackage 'Snafu' -InGroup 'Two'
+        GivenPackage 'Snafu' -InGroup 'Three'
+        WhenGettingPackage 'Snafu' -InGroup ''
+        ThenPackageReturned 'Snafu'
+        ThenNoErrors
+    }
 }
