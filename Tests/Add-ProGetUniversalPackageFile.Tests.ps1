61768739d1077b0e60d9c143fab2fd8414a05625
Aaron Jensen <ajensen@webmd.net>
2024-07-26 13:23:34 -0700

    Migrating tests to Pester 5.

--- Tests/Add-ProGetUniversalPackageFile.Tests.ps1
+++ Tests/Add-ProGetUniversalPackageFile.Tests.ps1
@@ -2,63 +2,60 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath '..\ProGetAutomation\Import-ProGetAutomation.ps1' -Resolve)
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
 
-$package = $null
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
 
-function GivenFile
-{
-    param(
-        [string[]]
-        $Path,
+    $script:testDirPath = $null
+    $script:testNum = 0
+    $script:package = $null
 
-        $Content
-    )
-
-    foreach( $pathItem in $Path )
+    function GivenFile
     {
-        $fullPath = Join-Path -Path $TestDrive.FullName -ChildPath $pathItem
+        param(
+            [string[]]
+            $Path,
 
-        $parentDir = $fullPath | Split-Path
-        if( -not (Test-Path -Path $parentDir -PathType Container) )
-        {
-            New-Item -Path $parentDir -ItemType 'Directory'
-        }
+            $Content
+        )
 
-        if( -not (Test-Path -Path $fullPath -PathType Leaf) )
+        foreach( $pathItem in $Path )
         {
-            New-Item -Path $fullPath -ItemType 'File'
-        }
+            $fullPath = Join-Path -Path $script:testDirPath -ChildPath $pathItem
 
-        if( $Content )
-        {
-            [IO.File]::WriteAllText($fullPath,$Content)
+            $parentDir = $fullPath | Split-Path
+            if( -not (Test-Path -Path $parentDir -PathType Container) )
+            {
+                New-Item -Path $parentDir -ItemType 'Directory'
+            }
+
+            if( -not (Test-Path -Path $fullPath -PathType Leaf) )
+            {
+                New-Item -Path $fullPath -ItemType 'File'
+            }
+
+            if( $Content )
+            {
+                [IO.File]::WriteAllText($fullPath,$Content)
+            }
         }
     }
-}
 
-function Init
-{
-    $script:package = $null
-}
-
-function ThenPackageContains
-{
-    param(
-        [string[]]
-        $EntryName,
-
-        $ExpectedContent
-    )
-
-    [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($package.FullName)
-    try
+    function ThenPackageContains
     {
-        It ('shouldn''t have duplicate entries') {
-            $file.Entries | Group-Object -Property 'FullName' | Where-Object { $_.Count -gt 1 } | Select-Object -ExpandProperty 'Group' | Should -HaveCount 0
-        }
+        param(
+            [string[]]
+            $EntryName,
 
-        It ('should add files to ZIP') {
+            $ExpectedContent
+        )
+
+        [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($script:package.FullName)
+        try
+        {
+            $file.Entries | Group-Object -Property 'FullName' | Where-Object { $_.Count -gt 1 } | Select-Object -ExpandProperty 'Group' | Should -HaveCount 0
+
             foreach( $entryNameItem in $EntryName )
             {
                 $entryNameItem = $entryNameItem -replace '\\','/'
@@ -79,252 +76,241 @@ function ThenPackageContains
                 }
             }
         }
-    }
-    finally
-    {
-        $file.Dispose()
-    }
-}
-
-function ThenPackageEmpty
-{
-    [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($package.FullName)
-    try
-    {
-        It ('should not add any files to ZIP') {
-            $file.Entries | Should -BeNullOrEmpty
+        finally
+        {
+            $file.Dispose()
         }
     }
-    finally
+
+    function ThenPackageEmpty
     {
-        $file.Dispose()
+        [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($script:package.FullName)
+        try
+        {
+            $file.Entries | Should -BeNullOrEmpty
+        }
+        finally
+        {
+            $file.Dispose()
+        }
     }
-}
 
-function ThenPackageNotContains
-{
-    param(
-        [string[]]
-        $Entry
-    )
-
-    [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($package.FullName)
-    try
+    function ThenPackageNotContains
     {
-        It ('should not add files to ZIP') {
+        param(
+            [string[]]
+            $Entry
+        )
+
+        [IO.Compression.ZipArchive]$file = [IO.Compression.ZipFile]::OpenRead($script:package.FullName)
+        try
+        {
             foreach( $entryName in $Entry )
             {
                 $file.GetEntry($entryName) | Should -BeNullOrEmpty
                 $file.GetEntry(('package\{0}' -f $entryName)) | Should -BeNullOrEmpty
             }
         }
+        finally
+        {
+            $file.Dispose()
+        }
     }
-    finally
+
+    function ThenError
     {
-        $file.Dispose()
-    }
-}
+        param(
+            $Matches
+        )
 
-function ThenError
-{
-    param(
-        $Matches
-    )
-
-    It ('should write an error') {
         $Global:Error | Should -Match $Matches
     }
-}
 
-function WhenAddingFiles
-{
-    [CmdletBinding()]
-    param(
-        [string[]]
-        $Path,
-
-        [Switch]
-        $Force,
-
-        $AtPackageRoot,
-
-        $WithBasePath,
-
-        $WithName,
-
-        [Switch]
-        $AsString,
-
-        [switch]
-        $Quiet,
-
-        [switch]
-        $NonPipeline
-    )
-
-    $packagePath = Join-Path -Path $TestDrive.FullName -ChildPath 'package.upack.zip'
-    if( -not (Test-Path -Path $packagePath -PathType Leaf) )
+    function WhenAddingFiles
     {
-        $script:package = New-ProGetUniversalPackage -OutFile $packagePath -Version '0.0.0' -Name 'ProGetAutomation'
-    }
+        [CmdletBinding()]
+        param(
+            [string[]]
+            $Path,
 
-    $params = @{
-        PackagePath = $package.FullName
-        Quiet = $Quiet
-    }
+            [Switch]
+            $Force,
 
-    if( $AtPackageRoot )
-    {
-        $params['PackageParentPath'] = $AtPackageRoot
-    }
+            $AtPackageRoot,
 
-    if( $Force )
-    {
-        $params['Force'] = $true
-    }
+            $WithBasePath,
 
-    if( $WithBasePath )
-    {
-        $params['BasePath'] = $WithBasePath
-    }
+            $WithName,
 
-    if( $WithName )
-    {
-        $params['PackageItemName'] = $WithName
-    }
+            [Switch]
+            $AsString,
 
-    $Global:Error.Clear()
+            [switch]
+            $Quiet,
 
-    $pathsToPackage =
-        $Path |
-        ForEach-Object { Join-Path -Path $TestDrive.FullName -ChildPath $_ } |
-        ForEach-Object {
-            if( $AsString )
-            {
-                $_
-            }
-            else
-            {
-                Get-Item -Path $_
-            }
+            [switch]
+            $NonPipeline
+        )
+
+        $packagePath = Join-Path -Path $script:testDirPath -ChildPath 'package.upack.zip'
+        if( -not (Test-Path -Path $packagePath -PathType Leaf) )
+        {
+            $script:package = New-ProGetUniversalPackage -OutFile $packagePath -Version '0.0.0' -Name 'ProGetAutomation'
         }
 
-    if( $NonPipeline )
-    {
-        Add-ProGetUniversalPackageFile -InputObject $pathsToPackage @params
-    }
-    else
-    {
-        $pathsToPackage | Add-ProGetUniversalPackageFile @params
+        $params = @{
+            PackagePath = $script:package.FullName
+            Quiet = $Quiet
+        }
+
+        if( $AtPackageRoot )
+        {
+            $params['PackageParentPath'] = $AtPackageRoot
+        }
+
+        if( $Force )
+        {
+            $params['Force'] = $true
+        }
+
+        if( $WithBasePath )
+        {
+            $params['BasePath'] = $WithBasePath
+        }
+
+        if( $WithName )
+        {
+            $params['PackageItemName'] = $WithName
+        }
+
+        $Global:Error.Clear()
+
+        $pathsToPackage =
+            $Path |
+            ForEach-Object { Join-Path -Path $script:testDirPath -ChildPath $_ } |
+            ForEach-Object {
+                if( $AsString )
+                {
+                    $_
+                }
+                else
+                {
+                    Get-Item -Path $_
+                }
+            }
+
+        if( $NonPipeline )
+        {
+            Add-ProGetUniversalPackageFile -InputObject $pathsToPackage @params
+        }
+        else
+        {
+            $pathsToPackage | Add-ProGetUniversalPackageFile @params
+        }
     }
 }
 
 Describe 'Add-ProGetUniversalPackageFile' {
-    Init
-    GivenFile 'one.cs','one.aspx','one.js','one.txt'
-    WhenAddingFiles '*.aspx','*.js'
-    ThenPackageContains 'one.aspx','one.js'
-    ThenPackageNotContains 'one.cs','one.txt'
-}
-
-Describe 'Add-ProGetUniversalPackageFile.when file already exists' {
-    Init
-    GivenFile 'one.cs' 'first'
-    WhenAddingFiles '*.cs'
-    GivenFile 'one.cs' 'second'
-    WhenAddingFiles '*.cs' -ErrorAction SilentlyContinue
-    ThenPackageContains 'one.cs' 'first'
-}
-
-Describe 'Add-ProGetUniversalPackageFile.when file already exists and forcing overwrite' {
-    Init
-    GivenFile 'one.cs' 'first'
-    WhenAddingFiles '*.cs'
-    GivenFile 'one.cs' 'second'
-    WhenAddingFiles '*.cs' -Force
-    ThenPackageContains 'one.cs' 'second'
-}
-
-Describe 'Add-ProGetUniversalPackageFile.when adding package root' {
-    Init
-    GivenFile 'one.cs'
-    WhenAddingFiles '*.cs' -AtPackageRoot 'package'
-    ThenPackageContains 'package\one.cs'
-    ThenPackageNotContains 'one.cs'
-}
-
-Describe 'Add-ProGetUniversalPackageFile.when passing path instead of file objects' {
-    Init
-    GivenFile 'one.cs','two.cs'
-    WhenAddingFiles 'one.cs', 'two.cs' -AsString
-    ThenPackageContains 'one.cs','two.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when changing name' {
-    Init
-    GivenFile 'one.cs'
-    WhenAddingFiles 'one.cs' -WithName 'cs.one'
-    ThenPackageContains 'cs.one'
-    ThenPackageNotContains 'one.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when passing a directory' {
-    Init
-    GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
-    WhenAddingFiles 'dir1'
-    ThenPackageContains 'dir1\one.cs','dir1\two.cs','dir1\three\four.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when customizing a directory name' {
-    Init
-    GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
-    WhenAddingFiles 'dir1' -WithName '1dir'
-    ThenPackageContains '1dir\one.cs','1dir\two.cs','1dir\three\four.cs'
-    ThenPackageNotContains 'dir1\one.cs','dir1\two.cs','dir1\three\four.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when passing a directory with a custom base path' {
-    Init
-    GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
-    WhenAddingFiles 'dir1' -WithBasePath (Join-Path -Path $TestDrive.FullName -ChildPath 'dir1')
-    ThenPackageContains 'one.cs','two.cs','three\four.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when piping filtered list of files' {
-    Init
-    GivenFile 'dir1\another\one.cs','dir1\another\two.cs'
-    $root = Join-Path -Path $TestDrive.FullName -ChildPath 'dir1'
-    WhenAddingFiles 'dir1\another\one.cs','dir1\another\two.cs' -AtPackageRoot 'dir2' -WithBasePath $root
-    ThenPackageContains 'dir2\another\one.cs','dir2\another\two.cs'
-}
-
-Describe 'Add-ZipArchiveEntry.when giving a direcotry a new root name' {
-    Init
-    GivenFile 'dir1\one.cs','dir1\two.cs'
-    WhenAddingFiles 'dir1\*.cs' -AtPackageRoot 'dir2'
-    ThenPackageContains 'dir2\one.cs','dir2\two.cs'
-}
-Describe 'Add-ZipArchiveEntry.when base path doesn''t match files' {
-    Init
-    GivenFile 'one.cs'
-    WhenAddingFiles 'one.cs' -WithBasePath 'C:\Windows\System32' -ErrorAction SilentlyContinue
-    ThenError -Matches 'is\ not\ in'
-}
-
-Describe 'Add-ZipArchiveEntry.when given Quiet switch' {
-    Init
-    Mock -CommandName 'Add-ZipArchiveEntry' -ModuleName 'ProGetAutomation'
-    GivenFile 'one.cs'
-    WhenAddingFiles 'one.cs' -Quiet
-
-    It 'should call Add-ZipArchiveEntry with Quiet switch' {
-        Assert-MockCalled -CommandName 'Add-ZipArchiveEntry' -ModuleName 'ProGetAutomation' -ParameterFilter { $Quiet.IsPresent }
+    BeforeEach {
+        $script:package = $null
+        $script:testDirPath = Join-Path -Path $TestDrive -ChildPath ($script:testNum++)
+        New-Item -Path $script:testDirPath -ItemType Directory
     }
-}
 
-Describe 'Add-ZipArchiveEntry.when passes files directly, in a non-pipeline manner' {
-    Init
-    GivenFile 'one.cs', 'two.cs'
-    WhenAddingFiles 'one.cs', 'two.cs' -NonPipeline
-    ThenPackageContains 'one.cs', 'two.cs'
+    It 'packages files' {
+        GivenFile 'one.cs','one.aspx','one.js','one.txt'
+        WhenAddingFiles '*.aspx','*.js'
+        ThenPackageContains 'one.aspx','one.js'
+        ThenPackageNotContains 'one.cs','one.txt'
+    }
+
+    It 'when package already contains file' {
+        GivenFile 'one.cs' 'first'
+        WhenAddingFiles '*.cs'
+        GivenFile 'one.cs' 'second'
+        WhenAddingFiles '*.cs' -ErrorAction SilentlyContinue
+        ThenPackageContains 'one.cs' 'first'
+    }
+
+    It 'when file already exists and forcing overwrite' {
+        GivenFile 'one.cs' 'first'
+        WhenAddingFiles '*.cs'
+        GivenFile 'one.cs' 'second'
+        WhenAddingFiles '*.cs' -Force
+        ThenPackageContains 'one.cs' 'second'
+    }
+
+    It 'when adding package root' {
+        GivenFile 'one.cs'
+        WhenAddingFiles '*.cs' -AtPackageRoot 'package'
+        ThenPackageContains 'package\one.cs'
+        ThenPackageNotContains 'one.cs'
+    }
+
+    It 'when passing path instead of file objects' {
+        GivenFile 'one.cs','two.cs'
+        WhenAddingFiles 'one.cs', 'two.cs' -AsString
+        ThenPackageContains 'one.cs','two.cs'
+    }
+
+    It 'when changing name' {
+        GivenFile 'one.cs'
+        WhenAddingFiles 'one.cs' -WithName 'cs.one'
+        ThenPackageContains 'cs.one'
+        ThenPackageNotContains 'one.cs'
+    }
+
+    It 'when passing a directory' {
+        GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
+        WhenAddingFiles 'dir1'
+        ThenPackageContains 'dir1\one.cs','dir1\two.cs','dir1\three\four.cs'
+    }
+
+    It 'when customizing a directory name' {
+        GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
+        WhenAddingFiles 'dir1' -WithName '1dir'
+        ThenPackageContains '1dir\one.cs','1dir\two.cs','1dir\three\four.cs'
+        ThenPackageNotContains 'dir1\one.cs','dir1\two.cs','dir1\three\four.cs'
+    }
+
+    It 'when passing a directory with a custom base path' {
+        GivenFile 'dir1\one.cs','dir1\two.cs', 'dir1\three\four.cs'
+        WhenAddingFiles 'dir1' -WithBasePath (Join-Path -Path $script:testDirPath -ChildPath 'dir1')
+        ThenPackageContains 'one.cs','two.cs','three\four.cs'
+    }
+
+    It 'when piping filtered list of files' {
+        GivenFile 'dir1\another\one.cs','dir1\another\two.cs'
+        $root = Join-Path -Path $script:testDirPath -ChildPath 'dir1'
+        WhenAddingFiles 'dir1\another\one.cs','dir1\another\two.cs' -AtPackageRoot 'dir2' -WithBasePath $root
+        ThenPackageContains 'dir2\another\one.cs','dir2\another\two.cs'
+    }
+
+    It 'when giving a direcotry a new root name' {
+        GivenFile 'dir1\one.cs','dir1\two.cs'
+        WhenAddingFiles 'dir1\*.cs' -AtPackageRoot 'dir2'
+        ThenPackageContains 'dir2\one.cs','dir2\two.cs'
+    }
+
+    It 'when base path doesn''t match files' {
+        GivenFile 'one.cs'
+        WhenAddingFiles 'one.cs' -WithBasePath 'C:\Windows\System32' -ErrorAction SilentlyContinue
+        ThenError -Matches 'is\ not\ in'
+    }
+
+    It 'when given Quiet switch' {
+        Mock -CommandName 'Add-ZipArchiveEntry' -ModuleName 'ProGetAutomation'
+        GivenFile 'one.cs'
+        WhenAddingFiles 'one.cs' -Quiet
+
+        Assert-MockCalled -CommandName 'Add-ZipArchiveEntry' `
+                          -ModuleName 'ProGetAutomation' `
+                          -ParameterFilter { $Quiet.IsPresent }
+    }
+
+    It 'when passes files directly, in a non-pipeline manner' {
+        GivenFile 'one.cs', 'two.cs'
+        WhenAddingFiles 'one.cs', 'two.cs' -NonPipeline
+        ThenPackageContains 'one.cs', 'two.cs'
+    }
 }
--- Tests/Get-ProGetAsset.Tests.ps1
+++ Tests/Get-ProGetAsset.Tests.ps1
@@ -1,112 +1,126 @@
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
 
-function GivenSession
-{
-    $script:session = New-ProGetTestSession
-    $script:baseDirectory = (split-path -Path $TestDrive.FullName -leaf)
-    $feed = Test-ProGetFeed -Session $session -Name $baseDirectory
-    if( !$feed )
+#Requires -Version 5.1
+Set-StrictMode -Version 'Latest'
+
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
+
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
+
+    $script:testNum = 0
+    $script:testDirPath = $null
+
+    function GivenSession
     {
-        New-ProGetFeed -Session $session -Name $baseDirectory -Type 'Asset'
-    }
-}
-
-function GivenAssets
-{
-    param(
-        [string[]]
-        $Name,
-        [string]
-        $WithContent = 'test'
-    )
-    foreach($file in $Name)
-    {
-        New-Item -Path (Join-Path -Path $TestDrive.FullName -ChildPath $file) -Type 'file' -value $WithContent -Force
-        Set-ProGetAsset -Session $session -DirectoryName $baseDirectory -Path $file -FilePath (Join-Path -Path $TestDrive.FullName -ChildPath $file)
-    }
-}
-
-function WhenAssetIsRequested
-{
-    param(
-        [string]
-        $Filter,
-        [string]
-        $Subdirectory
-    )
-    $Global:Error.Clear()
-    $script:assets = Get-ProGetAsset -Session $session -DirectoryName $baseDirectory -Path $Subdirectory -Filter $Filter -ErrorAction SilentlyContinue
-}
-
-function ThenListShouldBeReturned
-{
-    param(
-        [string[]]
-        $Name
-    )
-        it ('should return a list that matches ''{0}''' -f ($Name -join ''', ''')){
-            foreach($asset in $assets) { $Name | Where-Object { $_ -contains $asset.Name } | Should -not -BeNullOrEmpty }
-            foreach($item in $Name) { $assets | Where-Object { $_.name -contains $item} | Should -not -BeNullOrEmpty }
+        $script:session = New-ProGetTestSession
+        $script:baseDirectory = (split-path -Path $script:testDirPath -leaf)
+        $feed = Test-ProGetFeed -Session $session -Name $baseDirectory
+        if( !$feed )
+        {
+            New-ProGetFeed -Session $session -Name $baseDirectory -Type 'Asset'
         }
-}
+    }
 
-function ThenListShouldBeEmpty
-{
-    it 'should return a list that is empty' {
+    function GivenAssets
+    {
+        param(
+            [string[]]
+            $Name,
+            [string]
+            $WithContent = 'test'
+        )
+
+        foreach($file in $Name)
+        {
+            New-Item -Path (Join-Path -Path $script:testDirPath -ChildPath $file) -Type 'file' -value $WithContent -Force
+            Set-ProGetAsset -Session $session -DirectoryName $baseDirectory -Path $file -FilePath (Join-Path -Path $script:testDirPath -ChildPath $file)
+        }
+    }
+
+    function WhenAssetIsRequested
+    {
+        param(
+            [string]
+            $Filter,
+            [string]
+            $Subdirectory
+        )
+        $Global:Error.Clear()
+        $script:assets = Get-ProGetAsset -Session $session -DirectoryName $baseDirectory -Path $Subdirectory -Filter $Filter -ErrorAction SilentlyContinue
+    }
+
+    function ThenListShouldBeReturned
+    {
+        param(
+            [string[]]
+            $Name
+        )
+
+        foreach($asset in $assets) { $Name | Where-Object { $_ -contains $asset.Name } | Should -not -BeNullOrEmpty }
+        foreach($item in $Name) { $assets | Where-Object { $_.name -contains $item} | Should -not -BeNullOrEmpty }
+    }
+
+    function ThenListShouldBeEmpty
+    {
         $assets | Should -BeNullOrEmpty
     }
-}
-function ThenNoErrorShouldBeThrown
-{
-    It 'should not throw an error' {
+    function ThenNoErrorShouldBeThrown
+    {
         $Global:Error | Should -BeNullOrEmpty
     }
 }
 
-Describe 'Get-ProGetAsset.when list of assets is returned'{
-    GivenSession
-    GivenAssets -name 'foo','bar' -directory
-    WhenAssetIsRequested
-    ThenListShouldBeReturned -name 'foo','bar'
-    ThenNoErrorShouldBeThrown
-}
+Describe 'Get-ProGetAsset' {
+    BeforeEach {
+        $script:testDirPath = Join-Path -Path $TestDrive -ChildPath ($script:testNum++)
+        New-Item -Path $script:testDirPath -ItemType Directory
+    }
 
-Describe 'Get-ProGetAsset.when using wildcard'{
-    GivenSession
-    GivenAssets -name 'foo','foobar','notfbar'
-    WhenAssetIsRequested -filter '*foo*'
-    ThenListShouldBeReturned -name 'foo','foobar'
-    ThenNoErrorShouldBeThrown
-}
+    It 'when list of assets is returned'{
+        GivenSession
+        GivenAssets -name 'foo','bar' -directory
+        WhenAssetIsRequested
+        ThenListShouldBeReturned -name 'foo','bar'
+        ThenNoErrorShouldBeThrown
+    }
 
-Describe 'Get-ProGetAsset.when single asset is returned'{
-    GivenSession
-    GivenAssets -name 'foo.txt' -WithContent 'test'
-    WhenAssetIsRequested -filter 'foo.txt'
-    ThenListShouldBeReturned -name 'foo.txt'
-    ThenNoErrorShouldBeThrown
-}
+    It 'when using wildcard'{
+        GivenSession
+        GivenAssets -name 'foo','foobar','notfbar'
+        WhenAssetIsRequested -filter '*foo*'
+        ThenListShouldBeReturned -name 'foo','foobar'
+        ThenNoErrorShouldBeThrown
+    }
 
-Describe 'Get-ProGetAsset.when asset is requested but does not exist'{
-    GivenSession
-    GivenAssets -name 'foo' -withContent 'test content'
-    WhenAssetIsRequested -filter 'fubu'
-    ThenNoErrorShouldBeThrown
-    ThenListShouldBeEmpty
-}
+    It 'when single asset is returned'{
+        GivenSession
+        GivenAssets -name 'foo.txt' -WithContent 'test'
+        WhenAssetIsRequested -filter 'foo.txt'
+        ThenListShouldBeReturned -name 'foo.txt'
+        ThenNoErrorShouldBeThrown
+    }
 
-Describe 'Get-ProGetAsset.when list of assets is returned from a subdirectory'{
-    GivenSession
-    GivenAssets -name 'world/world.txt','world/hello.txt' -Directory 'hello'
-    WhenAssetIsRequested -subdirectory 'world'
-    ThenListShouldBeReturned -name 'world.txt','hello.txt'
-    ThenNoErrorShouldBeThrown
-}
+    It 'when asset is requested but does not exist'{
+        GivenSession
+        GivenAssets -name 'foo' -withContent 'test content'
+        WhenAssetIsRequested -filter 'fubu'
+        ThenNoErrorShouldBeThrown
+        ThenListShouldBeEmpty
+    }
 
-Describe 'Get-ProGetAsset.when list of assets is returned from a subdirectory with backslashes'{
-    GivenSession
-    GivenAssets -name '\world\world.txt','\world\hello.txt' -Directory '\hello\'
-    WhenAssetIsRequested -subdirectory 'world'
-    ThenListShouldBeReturned -name 'world.txt','hello.txt'
-    ThenNoErrorShouldBeThrown
+    It 'when list of assets is returned from a subdirectory'{
+        GivenSession
+        GivenAssets -name 'world/world.txt','world/hello.txt' -Directory 'hello'
+        WhenAssetIsRequested -subdirectory 'world'
+        ThenListShouldBeReturned -name 'world.txt','hello.txt'
+        ThenNoErrorShouldBeThrown
+    }
+
+    It 'when list of assets is returned from a subdirectory with backslashes'{
+        GivenSession
+        GivenAssets -name '\world\world.txt','\world\hello.txt' -Directory '\hello\'
+        WhenAssetIsRequested -subdirectory 'world'
+        ThenListShouldBeReturned -name 'world.txt','hello.txt'
+        ThenNoErrorShouldBeThrown
+    }
 }
