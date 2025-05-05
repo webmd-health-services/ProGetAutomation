d37dd6df4ab8c4ed6ee3c793f84dfa0cbc9f7cbf
Joseph Larionov <jlarionov@webmd.net>
2025-05-05 13:40:40 -0700

    Test against ProGet 2024.34

--- init.ps1
+++ init.ps1
@@ -19,7 +19,7 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'PSModules\Carbon'
 
 # TODO: When updating to 2024.35 or later, remove the "-Skip" parameter from the Set-ProGetAsset.Tests.ps1 test named:
 # 'should not overwrite assets imported from zip archive by default'
-$version = '24.0.16'
+$version = '24.0.34'
 
 $runningUnderAppVeyor = (Test-Path -Path 'env:APPVEYOR')
 
